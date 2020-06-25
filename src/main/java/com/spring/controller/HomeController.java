4028d3815317c0c521444c91daa8cbcdebaf2f41
sri <srilalitha18@gmail.com>
2020-06-25 10:48:22 +0530

    Spring MVC Hello world Example

---
+++ src/main/java/com/spring/controller/HomeController.java
@@ -0,0 +1,44 @@
+package com.spring.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spring.model.User;
+
+@Controller
+public class HomeController {
+
+	private static final Log logger = LogFactory.getLog(HomeController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Home Page Requested, locale = " + locale);
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+
+		return "home";
+	}
+
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	public String user(@Validated User user, Model model) {
+		logger.info("User Page Requested");
+		model.addAttribute("userName", user.getUserName());
+		return "user";
+	}
+}
